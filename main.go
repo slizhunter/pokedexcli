f8fce335bd1cbfd022a0d9c419c4433ce4dc8981
slizhunter <daniel.frazier@gmail.com>
2026-05-10 19:24:47 -0400

    Added very basic REPL functionality

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
-func main(){
-	fmt.Println("Hello, World!")
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		if scanSuccess := scanner.Scan(); !scanSuccess {
+			fmt.Println("input scanning error")
+		}
+		input := scanner.Text()
+		cleanedInput := cleanInput(input)
+		fmt.Printf("Your command was: %s\n", cleanedInput[0])
+	}
 }
